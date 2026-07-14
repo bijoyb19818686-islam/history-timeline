c33f243f8166657a36f9966e0d135cf3a990e48c
Bijoy Islam <bijoy.b19818686@gmail.com>
2026-07-14 15:17:50 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.lo
+const form=document.getElementById("eventForm");
+
+form.addEventListener("submit",function(e){
+
+e.preventDefault();
+
+const year=document.getElementById("year").value;
+const title=document.getElementById("title").value;
+const description=document.getElementById("description").value;
+
+const timeline=document.getElementById("timeline");
+
+timeline.innerHTML+=`
+<div class="timeline-item">
+<h3>${year} - ${title}</h3>
+<p>${description}</p>
+</div>
+`;
+
+form.reset();
+
+});
